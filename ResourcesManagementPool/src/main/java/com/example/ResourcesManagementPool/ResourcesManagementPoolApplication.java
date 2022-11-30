0e5b8500ba470caaa8767993c1c6118270f877aa
poc-demo-project <bharatkumar.nettem@hcl.com>
2022-11-30 12:01:32 +0530

    Phase2

---
+++ ResourcesManagementPool/src/main/java/com/example/ResourcesManagementPool/ResourcesManagementPoolApplication.java
@@ -0,0 +1,14 @@
+package com.example.ResourcesManagementPool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourcesManagementPoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourcesManagementPoolApplication.class, args);
+		System.out.println("Hello");
+	}
+
+}
---
+++ ResourcesManagementPool/src/test/java/com/example/ResourcesManagementPool/ResourcesManagementPoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ResourcesManagementPool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourcesManagementPoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
